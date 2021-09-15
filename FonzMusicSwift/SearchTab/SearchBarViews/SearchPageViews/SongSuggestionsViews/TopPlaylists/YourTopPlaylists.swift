83b7bb1d0b3cae5e1b78462aed5897d0141955ba
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2021-09-15 19:21:52 +0100

    changed top artists defaults

--- FonzMusicSwift/SearchTab/SearchBarViews/SearchPageViews/SongSuggestionsViews/TopPlaylists/YourTopPlaylists.swift
+++ FonzMusicSwift/SearchTab/SearchBarViews/SearchPageViews/SongSuggestionsViews/TopPlaylists/YourTopPlaylists.swift
@@ -24,20 +24,11 @@ struct YourTopPlaylists: View {
     
     @State var connectedToSpotify : Bool = false
     
-//    var topPlaylists =
-//    [
-//        Playlist(playlistName:  "This Is Rush",  playlistId: "37i9dQZF1DX9E92APFiTvV", playlistImage: "https://i.scdn.co/image/ab67706f0000000336834b90af842ac737f7dac3", amountOfTracks: 50),
-//        Playlist(playlistName: "memoryLane",  playlistId: "3sRM90oyy8Zul8iF3Cg3RF", playlistImage: "https://i.scdn.co/image/ab67706c0000bebbbbb8672f1afbe3add6554550", amountOfTracks: 83),
-//        Playlist(playlistName: "beautifulEscape",  playlistId: "4d2ObNuTa7AIMJY8TGvLDB", playlistImage: "https://i.scdn.co/image/ab67706c0000bebb12bc8c494507c2f6550b919b", amountOfTracks: 23),
-//        Playlist(playlistName:   "manifest destiny", playlistId: "4TOUWjjIiwBNXj82cLnltq", playlistImage: "https://i.scdn.co/image/ab67706c0000bebb802a02183d45115fc2639cb6", amountOfTracks: 234),
-//        Playlist(playlistName:  "malibuNinetyTwo", playlistId: "6rqm7IuR0DRbktih6FV9jm", playlistImage: "https://i.scdn.co/image/ab67706c0000bebb0e65fceaffd297b0f3f14756", amountOfTracks: 212)
-//
-//    ]
     
     var body: some View {
         VStack {
             HStack{
-                Text(connectedToSpotify ? "your top playlists" : "spotify top playlists")
+                Text(connectedToSpotify ? "your top playlists" : "fonz top playlists")
                     .foregroundColor(colorScheme == .light ? Color.darkBackground: Color.white)
                     .fonzParagraphTwo()
                     .padding(.horizontal, .subHeadingFrontIndent)
